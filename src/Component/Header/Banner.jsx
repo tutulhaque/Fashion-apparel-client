63dc194f0d6de0fb02358a4948333383dc2d232b
tutulhaque <sdtutulctg@gmail.com>
2023-10-19 23:29:39 +0600

    Navbar and footer and created

--- src/Component/Header/Banner.jsx
+++ src/Component/Header/Banner.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import HomeBanner from '../../assets/banner-home.png';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 const Banner = () => {
+
+    // Define the style object for the background image
+    const backgroundImageStyle = {
+        backgroundImage: `url(${HomeBanner})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    };
+
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in',
+            offset: 200, // Animation starts as soon as the element is in the viewport
+        });
+    }, []);
+
     return (
-        <div>
-            
+        <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/tsgF5yd/banner-home.png)' }}>
+            <div className=""></div>
+            <div className="hero-content text-center text-neutral-content">
+            </div>
         </div>
     );
 };
